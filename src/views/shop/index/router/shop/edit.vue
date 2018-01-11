0db3b241e4f49abc80746e2cf3c96156d6ed52f7
wjiaang <wjiaang@outlook.com>
2018-01-11 20:18:12 +0800

    同步商品和一键同步布局   判断流量包初始显示

--- src/views/shop/index/router/shop/edit.vue
+++ src/views/shop/index/router/shop/edit.vue
@@ -34,6 +34,13 @@
                 <div class="telephone-list" 
                         v-for="(Telephone,index) in Telephones"
                         :key="index">
+                        <!-- <el-input placeholder="请输入内容" v-model.number="Telephone.phone">
+                            <el-select v-model="select" slot="prepend" placeholder="请选择">
+                                <el-option label="餐厅名" value="1"></el-option>
+                                <el-option label="订单号" value="2"></el-option>
+                                <el-option label="用户电话" value="3"></el-option>
+                            </el-select>
+                        </el-input> -->
                     <el-input   class="el-telephone"
                             v-model="Telephone.phone" 
                             placeholder="请输入推送手机">
@@ -142,7 +149,7 @@ export default {
                 stoSmsTelephone: arr, 
                 stoQqCustomer: _this.form.stoQqCustomer
             }
-            
+
             //防止多次点击重复提交数据
             if(!Lib.C.ajax_manage) return false;
             Lib.C.ajax_manage = false;
